efb9217056e71ce3a40fc4d6a1e35a9638e95b50
webdiziner01 <webdiziner01@gmail.com>
2018-08-17 16:20:18 +0500

    commit on video 03

---
+++ resources/views/account/layouts/partials/_navigation.blade.php
@@ -0,0 +1,17 @@
+<nav class="nav nav-pills nav-stacked  flex-column ">
+
+    <li class="nav-item">
+        <a class="nav-link active" href="#">Active</a>
+    </li>
+
+
+    <li class="nav-item">
+        <a class="nav-link" href="{{route('account.index')}}">Account Overview</a>
+    </li>
+
+
+    <li class="nav-item">
+        <a class="nav-link" href="{{route('account.profile.index')}}">Profile</a>
+    </li>
+
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,20 @@ Auth::routes();
 Route::get('/', 'HomeController@index')->name('home');
 
 Route::get('dashboard','DashboardController@index');
+
+
+
+Route::group(['prefix'=> 'account', 'middleware' => ['auth'], 'as' => 'account.'], function (){
+    Route::get('/', 'Account\AccountController@index')->name('index');
+
+
+    /*
+     * Profile
+     * */
+    Route::get('/profile', 'Account\ProfileController@index')->name('profile.index');
+    Route::post('/profile', 'Account\ProfileController@store')->name('profile.store');
+
+
+
+
+});
